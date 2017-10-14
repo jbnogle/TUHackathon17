e4a7ff8e4e6402e1d3384c2b9b78e8981ce687a8
TheOatman <matthewoatman@gmail.com>
2017-10-14 07:33:15 -0700

    Add files via upload
    Accept Page + altered index to build link url

---
+++ howdy/views.py
@@ -0,0 +1,19 @@
+ # howdy/views.py
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+# Create your views here.
+class HomePageView(TemplateView):
+    template_name = "index.html"
+
+class AcceptPageView(TemplateView):
+    template_name = "accept.html"
+
+class StatsPageView(TemplateView):
+    template_name = "stats.html"
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
+
+class PopupPageView(TemplateView):
+    template_name = "about.html"
